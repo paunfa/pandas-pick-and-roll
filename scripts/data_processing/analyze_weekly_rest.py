12c5348afd85980d4f0c44ef6f3ea7316407d4f8
paunfa <psunga1999@gmail.com>
2026-07-23 02:46:22 -0400

    Added weekly rest analysis

--- scripts/data_processing/analyze_weekly_rest.py
+++ scripts/data_processing/analyze_weekly_rest.py
@@ -1,15 +1,65 @@
 """
-Convert game=level rest data into fantasy-week rest data.
+Analyzes weekly rest advantages for NBA teams.
+
+Creates weekly rest summaries used for fantasy streaming analysis.
 """
 
 import pandas as pd
-from utils.paths import RAW_DATA, PROCESSED_DATA
+
+from utils.paths import (
+    PROCESSED_TEAM_GAME_LOGS_PATH,
+    WEEKLY_REST_SUMMARY_PATH
+)
+
 
 def analyze_weekly_rest():
 
+    print("Analyzing weekly rest data...")
 
-def main()
+    team_game_logs = pd.read_csv(
+        PROCESSED_TEAM_GAME_LOGS_PATH
+    )
+
+    weekly_rest_summary = (
+        team_game_logs
+        .groupby(
+            [
+                "WEEK_START",
+                "TEAM_ABBREVIATION"
+            ]
+        )
+        .agg(
+            GAMES_THIS_WEEK=("GAME_ID", "count"),
+            BACK_TO_BACKS=("BACK_TO_BACK", "sum"),
+            AVERAGE_REST=("REST_DAYS", "mean"),
+            MIN_REST=("REST_DAYS", "min"),
+            MAX_REST=("REST_DAYS", "max")
+        )
+        .reset_index()
+    )
+
+    weekly_rest_summary["AVERAGE_REST"] = (
+        weekly_rest_summary["AVERAGE_REST"]
+        .round(2)
+    )
+
+    weekly_rest_summary.to_csv(
+        WEEKLY_REST_SUMMARY_PATH,
+        index=False
+    )
+
+    print(
+        f"\n========================================"
+        f"\nSuccessfully created weekly rest summary.")
+    print(
+        f"Saved to: {WEEKLY_REST_SUMMARY_PATH}"
+        f"\n========================================"
+    )
+
+
+def main():
     analyze_weekly_rest()
+
 
 if __name__ == "__main__":
     main()
